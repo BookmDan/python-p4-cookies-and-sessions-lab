cb7897ac8046731336a381b76a7ff43116a4e1b7
BookmDan <danoh329@gmail.com>
2023-12-28 04:58:34 -0800

    complete

--- server/app.py
+++ server/app.py
@@ -22,13 +22,22 @@ def clear_session():
 
 @app.route('/articles')
 def index_articles():
-
-    pass
+    return jsonify({'message': 'List of articles'}), 200
 
 @app.route('/articles/<int:id>')
 def show_article(id):
+    session['page_views'] = session.get('page_views',0)
+    session['page_views'] += 1
 
-    pass
+    if session['page_views']<=3:
+        article = Article.query.get(id)
+        if article:
+            return jsonify({'id': article.id, 'title': article.title, 'content': article.content}),200
+        else:
+            return jsonify({'message': 'Article not found.'}), 404
+    else:
+        return jsonify({'message': 'Maximum pageview limit reached'}), 401
+
 
 if __name__ == '__main__':
     app.run(port=5555)
